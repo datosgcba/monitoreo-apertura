98cc67328496580947676333c8efae7443b00b8f
franciclo <francisco@democracyos.io>
2019-01-16 00:20:43 -0300

    agrega primeros indicadores

--- src/indicadores.py
+++ src/indicadores.py
@@ -1,10 +1,10 @@
 # '/usr/src/append.txt'
 # '/home/franciclo/git/indicadores-portal-datos-abiertos/src/append.txt'
 
-import csv
 import requests
 import os
 from collections import Counter
+import pandas as pd
 
 url = ""
 
@@ -14,18 +14,26 @@ except:
   print "Falta DATA_JSON_URL"
   exit()
 
-r = requests.get(url)
-data_json = r.json()
+data_json = requests.get(url).json()
 
-cantidad_datasets = len(data_json.datasets)
 
-cantidad_datasets_org = Counter([ dataset.source.split('.')[0] for dataset in data_json.datasets ])
+# Calculo de indicadores
 
-cantidad_recursos_org = [ len(dataset.distribution) for dataset in data_json.datasets ] # hacerlo por org
+cantidad_datasets = len(data_json['dataset'])
 
-cantidad_datasets_cat = Counter([ theme for theme in dataset.theme for dataset in data_json.datasets ])
+cantidad_recursos = len(data_json['dataset']) # sumar distribuciones
 
-with open('/home/franciclo/git/indicadores-portal-datos-abiertos/src/indicadores.csv', mode='a') as indicadores:
-  indicadores = csv.writer(indicadores, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-  indicadores.writerow(['John Smith', 'Accounting', 'November'])
+cantidad_datasets_org = Counter([ dataset['source'].split('.')[0] for dataset in data_json['dataset'] ]) #esta mal lo del counter con objetos, no suma las keys iguales
+
+cantidad_recursos_org = Counter({ dataset['source'].split('.')[0]: len(dataset['distribution']) for dataset in data_json['dataset'] })
+
+cantidad_datasets_cat = Counter([ theme for theme in dataset['theme'] for dataset in data_json['dataset'] ])
+
+cantidad_recursos_cat = Counter({ theme: len(dataset['distribution']) for theme in dataset['theme'] for dataset in data_json['dataset'] })
+
+# Fin calculo de indicadores
+
+# indicadores = pd.read_csv('/home/franciclo/git/indicadores-portal-datos-abiertos/src/indicadores.csv')
+
+# indicadores[]
  
