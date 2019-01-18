45b806908cbe865d0790e9856c20c3bd3c5428f4
franciclo <francisco@democracyos.io>
2019-01-18 16:46:21 -0300

    correr el cron los lunes

--- src/cron.py
+++ src/cron.py
@@ -7,6 +7,6 @@ env_vars = { "data_json_url": os.environ["DATA_JSON_URL"], "source_path": os.env
 
 job = cron.new(command="DATA_JSON_URL={data_json_url} SOURCE_PATH={source_path} python {source_path}indicadores.py".format(**env_vars))
 
-job.minute.every(1) # hacerlo semanal
+job.dow.on('MON')
 
 cron.write()
