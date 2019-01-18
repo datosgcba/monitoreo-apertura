031a04ee13a50857f9ee8e5a5f91150a22ec38ac
franciclo <francisco@democracyos.io>
2019-01-17 23:55:50 -0300

    guardar indicadores en csv

--- src/cron.py
+++ src/cron.py
@@ -3,8 +3,8 @@ from crontab import CronTab
 
 cron = CronTab(True)
 
-job = cron.new(command='python /home/franciclo/git/indicadores-portal-datos-abiertos/src/indicadores.py')
-# job = cron.new(command='python /usr/src/indicadores.py')
+job = cron.new(command="python {}".format(os.path.abspath("indicadores.py")))
+
 job.minute.every(1) # hacerlo semanal
 
 cron.write()
--- src/indicadores.py
+++ src/indicadores.py
@@ -1,39 +1,59 @@
-# '/usr/src/append.txt'
-# '/home/franciclo/git/indicadores-portal-datos-abiertos/src/append.txt'
-
-import requests
+# coding=utf-8
 import os
-from collections import Counter
+import datetime
+import requests 
+import unidecode
 import pandas as pd
-
-url = ""
+from collections import Counter
 
 try:
-  url = os.environ['DATA_JSON_URL']
+  url = os.environ["DATA_JSON_URL"]
 except:
   print "Falta DATA_JSON_URL"
   exit()
 
 data_json = requests.get(url).json()
 
+def normalizeColumnName (string):
+  return "_".join([ filter(str.isalnum, s) for s in unidecode.unidecode(string.lower().replace(' ', '_').replace('-', '_')).split('_') ])
 
-# Calculo de indicadores
 
-cantidad_datasets = len(data_json['dataset'])
+# Cantidad de Datasets
+cantidad_datasets = len(data_json["dataset"])
 
-cantidad_recursos = len(data_json['dataset']) # sumar distribuciones
+# Cantidad de Datasets por organización
+cantidad_datasets_org = Counter([ normalizeColumnName('datasets_organizacion_' + dataset["source"].split(".")[0]) for dataset in data_json["dataset"] ])
 
-cantidad_datasets_org = Counter([ dataset['source'].split('.')[0] for dataset in data_json['dataset'] ]) #esta mal lo del counter con objetos, no suma las keys iguales
+# Cantidad de Recursos por organización
+# cantidad_recursos_org = Counter({ dataset["source"].split(".")[0]: len(dataset["distribution"]) for dataset in data_json["dataset"] }) # falta sumar las keys iguales
 
-cantidad_recursos_org = Counter({ dataset['source'].split('.')[0]: len(dataset['distribution']) for dataset in data_json['dataset'] })
+# Cantidad de Datasets por categoría
+cantidad_datasets_cat = Counter([ normalizeColumnName("datasets_categoria_" + theme) for theme in dataset["theme"] for dataset in data_json["dataset"] ])
 
-cantidad_datasets_cat = Counter([ theme for theme in dataset['theme'] for dataset in data_json['dataset'] ])
+# Cantidad de Recursos por categoría
+# cantidad_recursos_cat = Counter({ theme: len(dataset["distribution"]) for theme in dataset["theme"] for dataset in data_json["dataset"] }) # falta sumar las keys iguales
 
-cantidad_recursos_cat = Counter({ theme: len(dataset['distribution']) for theme in dataset['theme'] for dataset in data_json['dataset'] })
+# Cantidad total de recursos
+# cantidad_recursos = len(data_json["dataset"]) # falta sumar distribuciones
 
-# Fin calculo de indicadores
 
-# indicadores = pd.read_csv('/home/franciclo/git/indicadores-portal-datos-abiertos/src/indicadores.csv')
+indicadores = pd.read_csv(os.path.abspath("src/indicadores.csv"))
 
-# indicadores[]
- 
+for org in cantidad_datasets_org:
+  if org not in indicadores.columns:
+    indicadores.insert(0, org, "")
+
+for cat in cantidad_datasets_cat:
+  if cat not in indicadores.columns:
+    indicadores.insert(0, cat, "")
+
+nuevos_valores = {
+  'cantidad_datasets': cantidad_datasets,
+  'fecha': datetime.datetime.now().strftime("%d/%m/%Y")
+}
+nuevos_valores.update(cantidad_datasets_org)
+nuevos_valores.update(cantidad_datasets_cat)
+
+indicadores = indicadores.append(nuevos_valores, ignore_index=True)
+
+indicadores.to_csv(os.path.abspath("src/indicadores.csv"), index=False)
