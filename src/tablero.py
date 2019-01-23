5697e53511b45b7335c6f722bd1143b7826e3759
franciclo <francisco@democracyos.io>
2019-01-23 17:06:52 -0300

    agrega bastrap

--- src/tablero.py
+++ src/tablero.py
@@ -6,13 +6,58 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.graph_objs as go
 
+import os
 indicadores = pd.read_csv("{}indicadores.csv".format(os.environ["SOURCE_PATH"]))
 indicadores['fecha'] = pd.to_datetime(indicadores['fecha'], format='%d/%m/%Y:%H:%M:%S', utc=True)
 
-cantidad_datasets_org = [column for column in indicadores.columns if column.startswith('datasets_organizacion_')]
-cantidad_datasets_cat = [column for column in indicadores.columns if column.startswith('datasets_categoria_')]
+app = dash.Dash(__name__, app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP]))
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+  <head>
+    {%metas%}
+    <title>Monitoreo de apertura de datos</title>
+    <link rel="icon" type="image/png" href="/assets/images/favicon.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    {%css%}
+  </head>
+  <body>
+    <header id="header">
+      <div class="border-gradient">
+        <div class="container">
+          <div id="logo-sitio">
+            <a href="#" style="text-decoration: none;">
+              <div id="ba-logo" class="navbar-brand img-responsive" data-original-title="" title=""></div>
+            </a>
+          </div>
+          <div id="nombre-sitio">
+            <a href="http://disfrutemosba.buenosaires.gob.ar">
+              <h1>Monitoreo de apertura de datos</h1>
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+    {%app_entry%}
+    <footer id="ba-footer">
+      <div class="container">
+        <div class="col-md-12 col-sm-12" style="margin-top: -55px;">
+          <div class="container-ciudad">
+            <a href="http://www.buenosaires.gob.ar/"><img src="/assets/images/logo-bac.svg" class="img-responsive logo_footer_ciudad"></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+    {%config%}
+    {%scripts%}
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
+'''
 
 app.layout = html.Div(children=[
   dbc.Container([
