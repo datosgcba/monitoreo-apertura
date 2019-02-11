8930216f409479232f388410189963df7de44bac
franciclo <francisco@democracyos.io>
2019-02-11 18:30:41 -0300

    update app.py

--- source/app.py
+++ source/app.py
@@ -6,12 +6,16 @@ import tablero
 import pandas as pd
 from multiprocessing import Process
 
+if "INDICADORES_CSV_URL" not in os.environ:
+  print("ERROR Falta la variables de entorno: INDICADORES_CSV_URL")
+  exit()
+
 app = dash.Dash()
 
 template = open("template.html", "r") 
 app.index_string = template.read()
 
-indicadores = pd.read_csv(os.environ["INDICADORES_CSV"])
+indicadores = pd.read_csv(os.environ["INDICADORES_CSV_URL"])
 indicadores['fecha'] = pd.to_datetime(indicadores['fecha'], format='%d/%m/%Y:%H:%M:%S', utc=True)
 
 app.layout = tablero.layout(indicadores)
