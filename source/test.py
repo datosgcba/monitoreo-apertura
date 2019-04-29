5a84337f369b53ae58941541e0fcfdb2fe077331
franciclo <francisco@democracyos.io>
2019-04-29 10:33:35 -0300

    arregla data.json duplicados por dia y borra json antes de escribir

--- source/test.py
+++ source/test.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import yaml
 import datetime
+from datetime import date
 import requests
 from pymongo import MongoClient
 from ga import updateDatajson, getBusquedas, getGaData
@@ -9,25 +10,25 @@ config = yaml.full_load(open("./config.yml", 'r'))
 connection = MongoClient(config['mongo_url'])
 db = connection['monitoreo-apertura']
 
-fecha = datetime.datetime.utcnow()
-ga_data = getGaData()
+def loadTestData():
+  fecha = datetime.datetime.utcnow()
+  ga_data = getGaData()
 
-data_json = requests.get(config['data_json']).json()
+  data_json = requests.get(config['data_json']).json()
 
-for dataset in data_json['dataset']:
-  dataset['modified'] = datetime.datetime.strptime(dataset['modified'], '%Y-%m-%dT%H:%M:%S.%f')
-  for distribution in dataset['distribution']:
-    if 'modified' in distribution.keys():
-      distribution['modified'] = datetime.datetime.strptime(distribution['modified'], '%Y-%m-%dT%H:%M:%S.%f')
-    
-data_json = updateDatajson(ga_data, data_json)
+  for dataset in data_json['dataset']:
+    dataset['modified'] = datetime.datetime.strptime(dataset['modified'], '%Y-%m-%dT%H:%M:%S.%f')
+    for distribution in dataset['distribution']:
+      if 'modified' in distribution.keys():
+        distribution['modified'] = datetime.datetime.strptime(distribution['modified'], '%Y-%m-%dT%H:%M:%S.%f')
+      
+  data_json = updateDatajson(ga_data, data_json)
 
-end = datetime.datetime.utcnow()
-dt = end - datetime.timedelta(days=120)
-step = datetime.timedelta(days=1)
+  end = datetime.datetime.utcnow()
+  dt = end - datetime.timedelta(days=120)
+  step = datetime.timedelta(days=1)
 
-while dt < end:
-  data_json['fecha'] = dt
-  db['data-json'].insert_one(data_json.copy())
-  dt += step
-
+  while dt < end:
+    data_json['fecha'] = dt
+    db['data-json'].insert_one(data_json.copy())
+    dt += step
